a66beea0b80acaebdeb5e13879489a3a693dfc1f
LalitSur <LalitSur@DESKTOP-7LERR5M>
2020-01-21 08:00:03 +0530

    Selenium test committed

---
+++ Print-git/src/Print_git_pkg/Print_git.java
@@ -0,0 +1,10 @@
+package Print_git_pkg;
+
+public class Print_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("added project to git");
+	}
+
+}
